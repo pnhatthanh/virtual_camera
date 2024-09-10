c8dfeef0ba113cffab78be824ee2c06bf4227830
nhatthanhdev <phamnhatthanh2212@gmail.com>
2024-09-10 22:12:19 +0700

    ui and ipcam

--- app/src/main/java/com/pbl/virtualcam/MainActivity.java
+++ app/src/main/java/com/pbl/virtualcam/MainActivity.java
@@ -1,12 +1,18 @@
 package com.pbl.virtualcam;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button startConnectButton = findViewById(R.id.start_connect);
+        TextView ipAddressTextView = findViewById(R.id.ip_address);
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ipString = String.format("%d.%d.%d.%d",
+                            (ipAddress & 0xff),
+                            (ipAddress >> 8 & 0xff),
+                            (ipAddress >> 16 & 0xff),
+                            (ipAddress >> 24 & 0xff));
+        ipAddressTextView.setText(ipString);
+
     }
 }
