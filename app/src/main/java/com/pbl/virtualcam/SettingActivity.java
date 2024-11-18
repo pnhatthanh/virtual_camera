e3a7d4cac1d50a50a80a21603740e797b2db1ecb
nhatthanhdev <phamnhatthanh2212@gmail.com>
2024-11-18 20:57:04 +0700

    ...

--- app/src/main/java/com/pbl/virtualcam/SettingActivity.java
+++ app/src/main/java/com/pbl/virtualcam/SettingActivity.java
@@ -130,9 +130,9 @@ public class SettingActivity extends AppCompatActivity {
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
         builder.setTitle("Các thiết bị kết nối");
         builder.setItems(devices.toArray(new String[0]),(dialog,which)->{
-            new AlertDialog.Builder(this)
-                    .setTitle("Ngắt kết nối")
-                    .setMessage("Bạn có muốn ngắt kết nối với thiết bị " + devices.get(which) + " không?")
+            AlertDialog.Builder confirmBuilder=new AlertDialog.Builder(this);
+            confirmBuilder.setTitle("Ngắt kết nối");
+            confirmBuilder.setMessage("Bạn có muốn ngắt kết nối với thiết bị " + devices.get(which) + " không?")
                     .setPositiveButton("Có", (dialog1, which1) -> {
                         for (SocketHandler client : SocketManager.socketSet) {
                             if (client.getClientAddress().equals(devices.get(which))) {
@@ -140,13 +140,22 @@ public class SettingActivity extends AppCompatActivity {
                                 break;
                             }
                         }
+                        showDevice();
                     })
                     .setNegativeButton("Không", (dialog1, which1) -> {
                         dialog1.dismiss();
-                    })
-                    .show();
+                        AlertDialog parentDialog=builder.create();
+                        parentDialog.show();
+                    });
+            confirmBuilder.create().show();
+        });
+        builder.setPositiveButton("OK",(dia,w)->{
+            dia.dismiss();
         });
         AlertDialog dialog = builder.create();
+        dialog.setOnDismissListener(d->{
+            tvEquipments.setText(SocketManager.socketSet.size()+" thiết bị");
+        });
         dialog.show();
     }
 }
